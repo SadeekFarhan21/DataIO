0f28db964f4d28d71755e2a59c581288758b47c6
SadeekFarhan21 <farhansadeek019@gmail.com>
2024-02-24 15:44:31 -0500

    update

--- Dashboard.py
+++ Dashboard.py
@@ -6,18 +6,20 @@ import streamlit as st
 st.set_page_config(
     page_title='Data IO 2024',
     page_icon=":bar_chart:",
-    layout="wide"
+    layout="wide",
 )
-
 # Title and styling
 st.title(':bar_chart: Bike Data Analytics Analytics')
 st.markdown('<style>div.block-container{padding-top: 1rem;}</style>', unsafe_allow_html=True)
 st.markdown('Most popular rides across Chicago')
-st.image('0000fa.png')
-st.code('')
-st.image('000052.png')
-st.image('000020.png')
-st.image('image.png')
-st.image('avgdurationcool.png')
-st.image('peakhours2.png')
-st.image('000010.png')
+
+
+st.title('Centered Images')
+
+st.image('0000fa.png', use_column_width=True)
+st.image('000052.png', use_column_width=True)
+st.image('000020.png', use_column_width=True)
+st.image('image.png', use_column_width=True)
+st.image('avgdurationcool.png', use_column_width=True)
+st.image('peakhours2.png', use_column_width=True)
+st.image('000010.png', use_column_width=True)
